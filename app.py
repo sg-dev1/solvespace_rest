c0b64ef586b6959d2b154f84040a41616edd268f
Stefan G <sg-dev1@users.noreply.github.com>
2024-03-29 19:29:25 +0100

    First minimal version partly implemented

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from slvs_helper import Slvs_Helper
 
 app = Flask(__name__)
+CORS(app)
 
-slvs_helper = Slvs_Helper()
+my_slvs_helper = Slvs_Helper()
+
+"""
+export interface SolverRequestType {
+    workplane: string;
+    entities: {id: number, t: "point" | "line" | "circle" | "arc", v: number[]}[];
+    constraints: {id: number, t: SlvsConstraints, v: number[]}[];
+}
+
+export interface SolverResponseType {
+    code: number;
+    failed: number[];
+    entities: {id: number, t: "point" | "line" | "circle" | "arc", v: number[]}[];
+    dof: number;
+}
+"""
 
 # POST-Endpunkt
-@app.route('/postdata', methods=['POST'])
+@app.route('/solve', methods=['POST'])
 def post_data():
+    global my_slvs_helper
     # Überprüfen, ob die Anfrage JSON-Daten enthält
     if request.is_json:
         # JSON-Daten aus der Anfrage erhalten
@@ -16,6 +34,31 @@ def post_data():
         # Hier kannst du mit den empfangenen Daten arbeiten
         # Zum Beispiel könntest du sie in die Konsole drucken
         print("Empfangene Daten:", data)
+
+        for entity in data["entities"]:
+            if entity["t"] == "point":
+                my_slvs_helper.addPoint(data["workplane"], entity["id"], entity["v"])
+            elif entity["t"] == "line":
+                my_slvs_helper.addLine(data["workplane"], entity["id"], entity["v"])
+            else:
+                print("Warning entity of type %s not yet supported" % entity.t)
+        for constraint in data["constraints"]:
+            my_slvs_helper.addConstraint(data["workplane"], constraint["id"], constraint["t"], constraint["v"])
+
+        result, a, b = my_slvs_helper.solve()
+
+        # TODO generate the proper result
+        print("result=%d" % result)
+        if result == 0:
+            print("dof=%d" % a)
+            print("changed entities")
+            print(b)
+        else:
+            print("failed:")
+            print(a)
+
+        # hack to reset the state of the solver, to be changed
+        my_slvs_helper = Slvs_Helper()
 
         # Beispielantwort zurückgeben
         return jsonify({"message": "Daten empfangen"}), 200
--- slvs_helper.py
+++ slvs_helper.py
@@ -1,9 +1,16 @@
 import slvs
 
+# Taken from slvs.h
+SLVS_RESULT_OKAY=0
+SLVS_RESULT_INCONSISTENT=1
+SLVS_RESULT_DIDNT_CONVERGE=2
+SLVS_RESULT_TOO_MANY_UNKNOWNS=3
+
 class Slvs_Helper():
     def __init__(self):
         self.sys = slvs.System()
         self.workPlaneGroup = 1
+        self.solveGroup = 2
         # entity ids
         self.xyPlaneId = 1
         self.xzPlaneId = 2
@@ -17,6 +24,10 @@ class Slvs_Helper():
         self._genXyPlane()
         self._genXzPlane()
         self._genYzPlane()
+
+        self._paramId = 100
+        self._entityIdBase = 100
+        self._constraintIdBase = 1
 
     def _genOrigin(self):
         # origin (x y z) = (0 0 0) of coordinate system
@@ -55,3 +66,78 @@ class Slvs_Helper():
         self.sys.addParam(slvs.makeParam(15, self.workPlaneGroup, q[3]))
         self.sys.addEntity(slvs.makeNormal3d(self.yzNormalId , self.workPlaneGroup, 12, 13, 14, 15))
         self.sys.addEntity(slvs.makeWorkplane(self.yzPlaneId, self.workPlaneGroup, self.originId, self.yzNormalId ))
+
+    def _planeStrToPlaneId(self, plane):
+        planeId = -1
+        if plane == "xy":
+            planeId = self.xyPlaneId
+        elif plane == "xz":
+            planeId = self.xzPlaneId
+        elif plane == "yz":
+            planeId = self.yzPlaneId
+        return planeId
+
+    def addPoint(self, plane, idHint, data):
+        planeId = self._planeStrToPlaneId(plane)
+
+        # TODO either here we must select the proper points (according to plane) or the frontend needs to do it
+
+        p1Id = self._paramId
+        self.sys.addParam(slvs.makeParam(p1Id, self.solveGroup, data[0]))
+        self._paramId = self._paramId + 1
+        p2Id = self._paramId
+        self.sys.addParam(slvs.makeParam(p2Id, self.solveGroup, data[1]))
+        self._paramId = self._paramId + 1
+        #p3Id = self._paramId
+        #self.sys.addParam(slvs.makeParam(p3Id, self.solveGroup, data[2]))
+        #self._paramId = self._paramId + 1
+        #self.sys.addEntity(slvs.makePoint3d(idHint + self._entityIdBase, self.solveGroup, planeId, p1Id, p2Id, p3Id))
+
+        print("make point with id %d and coords (%.3f, %.3f, %.3f)" % (idHint, data[0], data[1], data[2]))
+
+        self.sys.addEntity(slvs.makePoint2d(idHint + self._entityIdBase, self.solveGroup, planeId, p1Id, p2Id))
+
+    def addLine(self, plane, idHint, data):
+        planeId = self._planeStrToPlaneId(plane)
+
+        print("add line with id %d and data %s" % (idHint, str(data)))
+
+        self.sys.addEntity(
+            slvs.makeLineSegment(idHint + self._entityIdBase, self.solveGroup, planeId, data[0] + self._entityIdBase, data[1] + self._entityIdBase)
+            )
+    
+    def addConstraint(self, plane, idHint, constraintType, data):
+        planeId = self._planeStrToPlaneId(plane)
+
+        print("add constraint with id %d and type %d and data %s" % (idHint, constraintType, str(data)))
+
+        valA = data[0]
+        ptA = 0
+        if data[1] != 0:
+            ptA = data[1] + self._entityIdBase
+        ptB = 0
+        if data[2] != 0:
+            ptB = data[2] + self._entityIdBase
+        entityA = 0
+        if data[3] != 0:
+            entityA = data[3] + self._entityIdBase
+        entityB = 0
+        if data[4] != 0:
+            entityB = data[4] + self._entityIdBase
+
+        print("\t%.3f, %d, %d, %d, %d" % (valA, ptA, ptB, entityA, entityB))
+
+        self.sys.addConstraint(
+            slvs.makeConstraint(idHint + self._constraintIdBase, self.solveGroup, constraintType, planeId, valA, ptA, ptB, entityA, entityB)
+            )
+
+    def solve(self):
+        result = self.sys.solve(self.solveGroup, True)
+
+        if result == SLVS_RESULT_OKAY:
+            # TODO parse the changed coordinates
+            changedEntities = []
+            return result, self.sys.Dof, changedEntities
+        else:
+            # result code + list of ids of failed constraints
+            return result, self.sys.Failed, []
